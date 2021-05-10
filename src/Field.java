82b60e362021ac47845e5c2e65285c108f40d1f9
Timofey <froepamtima@gmail.com>
2021-05-10 11:44:26 +0300

    added all JPanel methods

--- src/Field.java
+++ src/Field.java
@@ -14,4 +14,106 @@ import javax.swing.Timer;
 
 @SuppressWarnings("serial")
 public class Field extends JPanel {
+    private double dragOffsetX;
+    private double dragOffsetY;
+
+    // Флаг приостановленности движения
+    private boolean paused;
+    private boolean paused1;
+    boolean isDragged;
+    // Динамический список скачущих мячей
+    private ArrayList<BouncingBall> balls = new ArrayList<BouncingBall>(10);
+    //private rectangle rect = new rectangle(null);
+
+    // Класс таймер отвечает за регулярную генерацию событий ActionEvent
+    // При создании его экземпляра используется анонимный класс,
+    // реализующий интерфейс ActionListener
+    private Timer repaintTimer = new Timer(10, new ActionListener() {
+        public void actionPerformed(ActionEvent ev) {
+            // Задача обработчика события ActionEvent - перерисовка окна
+            repaint();
+        }
+    });
+    // Конструктор класса BouncingBall
+    public Field() {
+        // Установить цвет заднего фона белым
+        setBackground(Color.WHITE);
+        // Запустить таймер
+        repaintTimer.start();
+    }
+
+    // Унаследованный от JPanel метод перерисовки компонента
+    public void paintComponent(Graphics g) {
+        // Вызвать версию метода, унаследованную от предка
+        super.paintComponent(g);
+        Graphics2D canvas = (Graphics2D) g;
+        // Последовательно запросить прорисовку от всех мячей из списка
+        for (BouncingBall ball: balls) {
+            ball.paint(canvas);
+        }
+
+        Graphics2D path = (Graphics2D) g;
+        rectangle.paint(path);
+    }
+
+    // Метод добавления нового мяча в список
+    public void addBall() {
+        //Заключается в добавлении в список нового экземпляра BouncingBall
+        // Всю инициализацию положения, скорости, размера, цвета
+        // BouncingBall выполняет сам в конструкторе
+        balls.add(new BouncingBall(this));
+        addMouseMotionListener(new MouseMotionHandler());
+        addMouseListener(new MouseHandler());
+    }
+
+    // Метод синхронизированный, т.е. только один поток может
+    // одновременно быть внутри
+    public  void pause() {
+        // Включить режим паузы
+        paused = true;
+    }
+
+    public  void pause1() {
+        // Включить режим паузы
+        paused1 = true;
+    }
+
+    // Метод синхронизированный, т.е. только один поток может
+    // одновременно быть внутри
+    public synchronized void resume() {
+        // Выключить режим паузы
+        paused = false;
+        paused1 = false;
+        // Будим все ожидающие продолжения потоки
+        notifyAll();
+    }
+
+    public boolean Proverka(){
+        boolean mark = false;
+        for (BouncingBall ball: balls) {
+            if(ball.getRadius() < 10)
+            {
+                mark =  true;
+                break;
+            }
+            else
+                mark =  false;
+        }
+        return mark;
+    }
+    // Синхронизированный метод проверки, может ли мяч двигаться
+    // (не включен ли режим паузы?)
+    public synchronized void canMove(BouncingBall ball) throws
+            InterruptedException {
+        if (paused) {
+            if (ball.getRadius() < 10)
+                wait();
+
+            // Если режим паузы включен, то поток, зашедший
+            // внутрь данного метода, засыпает
+            //wait();
+        }
+        if(paused1)
+            wait();
+    }
 }
