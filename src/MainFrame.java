37861d7d11afc0df56f99fdfaade8ee207c13a91
Timofey <froepamtima@gmail.com>
2021-05-10 11:33:54 +0300

    added src and MainFrame

---
+++ src/MainFrame.java
@@ -0,0 +1,19 @@
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import java.awt.BorderLayout;
+
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame {
+    public static void main(String[] args) {
+        // Создать и сделать видимым главное окно приложения
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        }
+}
